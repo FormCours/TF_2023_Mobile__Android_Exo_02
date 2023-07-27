2da4568867aad8d134722dd403f1f15909dd30e8
aurelien.strimelle@bstorm.be <aurelien.strimelle@bstorm.be>
2023-07-27 17:06:47 +0200

    Initial commit

---
+++ app/src/main/java/be/tftic/web2023/exo02/CallActivity.kt
@@ -0,0 +1,11 @@
+package be.tftic.web2023.exo02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call)
+    }
+}
---
+++ app/src/main/java/be/tftic/web2023/exo02/InfoActivity.kt
@@ -0,0 +1,11 @@
+package be.tftic.web2023.exo02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
