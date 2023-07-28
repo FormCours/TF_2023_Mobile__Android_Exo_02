0d885bdf97192fade8c4de8448ebde5e1d96bf40
aurelien.strimelle@bstorm.be <aurelien.strimelle@bstorm.be>
2023-07-28 12:24:24 +0200

    Open InfoActivity and CallActivity

--- app/src/main/java/be/tftic/web2023/exo02/MainActivity.kt
+++ app/src/main/java/be/tftic/web2023/exo02/MainActivity.kt
@@ -1,17 +1,20 @@
 package be.tftic.web2023.exo02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import be.tftic.web2023.exo02.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val EXTRA_USERNAME = "Username"
     }
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var username: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +24,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Obtenir les données depuis l'intent
-        val username : String? = intent.getStringExtra(EXTRA_USERNAME)
+        username = intent.getStringExtra(EXTRA_USERNAME) ?: getString(R.string.unknown_username)
 
         // Welcome message
-        Toast.makeText(this, getString(R.string.toast_welcome_msg).format(username), Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            this,
+            getString(R.string.toast_welcome_msg).format(username),
+            Toast.LENGTH_LONG
+        ).show()
+
+        // Listener des boutons
+        binding.btnMainInfo.setOnClickListener(this)
+        binding.btnMainCall.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+            when (v?.id) {
+                R.id.btn_main_info -> openActivity(InfoActivity::class.java)
+                R.id.btn_main_call -> openActivity(CallActivity::class.java)
+            }
+    }
+
+    private fun openActivity(cls: Class<*>) {
+        // Le parametre "cls" permet de récuperer l'activité à lancer
+        // Le type de donnée "Class<*>" represente n'importe quelle classe Java (Générique)
+
+        val intent : Intent = Intent(this, cls)
+        startActivity(intent)
     }
 }
